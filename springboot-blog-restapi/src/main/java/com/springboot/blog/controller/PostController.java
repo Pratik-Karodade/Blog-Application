64cda916882f3eab57eda752c4f0c705ec8847e2
Pratik-Karodade <karodadepratik@gmail.com>
2024-07-08 08:51:38 +0530

    Get all posts controller created

--- springboot-blog-restapi/src/main/java/com/springboot/blog/controller/PostController.java
+++ springboot-blog-restapi/src/main/java/com/springboot/blog/controller/PostController.java
@@ -1,17 +1,15 @@
 package com.springboot.blog.controller;
 
-import com.springboot.blog.entity.Post;
 import com.springboot.blog.payload.PostDto;
 import com.springboot.blog.service.PostService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/post")
+@RequestMapping("/api/posts")
 public class PostController {
 
     private PostService postService;
@@ -20,8 +18,15 @@ public class PostController {
         this.postService = postService;
     }
 
+    // url : http://localhost:8080/api/posts
     @PostMapping
     public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto){
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
+    }
+
+    // url : http://localhost:8080/api/posts/allposts
+    @GetMapping("/allposts")
+    public List<PostDto> getAllPosts(){
+        return postService.getAllPosts();
     }
 }
--- springboot-blog-restapi/src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
+++ springboot-blog-restapi/src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
@@ -6,6 +6,9 @@ import com.springboot.blog.repository.PostRepository;
 import com.springboot.blog.service.PostService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class PostServiceImpl implements PostService {
 
@@ -18,20 +21,34 @@ public class PostServiceImpl implements PostService {
     @Override
     public PostDto createPost(PostDto postDto) {
         //converting postdto to post
+        Post post = mapToEntity(postDto);
+        Post newPost = postRepository.save(post);
+        // convert post to postdto
+        PostDto postResponse = mapToDto(post);
+        return postResponse;
+    }
+
+    @Override
+    public List<PostDto> getAllPosts() {
+        List<Post> posts = postRepository.findAll();
+       return  posts.stream().map(this::mapToDto).collect(Collectors.toList());
+    }
+
+    //convert post to postdto
+    private PostDto mapToDto(Post post) {
+        PostDto postDto = new PostDto();
+        postDto.setTitle(post.getTitle());
+        postDto.setDescription(post.getDescription());
+        postDto.setContent(post.getContent());
+        return postDto;
+    }
+
+    private Post mapToEntity(PostDto postDto) {
         Post post = new Post();
         post.setContent(postDto.getContent());
         post.setDescription(postDto.getDescription());
         post.setId(postDto.getId());
         post.setTitle(postDto.getTitle());
-
-        Post newPost = postRepository.save(post);
-
-        // convert post to postdto
-        PostDto postResponse = new PostDto();
-        postResponse.setContent(newPost.getContent());
-        postResponse.setDescription(newPost.getDescription());
-        postResponse.setId(newPost.getId());
-        postResponse.setTitle(newPost.getTitle());
-        return postResponse;
+        return post;
     }
 }
