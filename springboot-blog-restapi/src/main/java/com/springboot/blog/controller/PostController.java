1f179180b4f425ba54b16df2242ea22c66def8b3
Pratik-Karodade <karodadepratik@gmail.com>
2024-07-19 08:55:40 +0530

    sorting implemented implemented

--- springboot-blog-restapi/src/main/java/com/springboot/blog/controller/PostController.java
+++ springboot-blog-restapi/src/main/java/com/springboot/blog/controller/PostController.java
@@ -30,9 +30,11 @@ public class PostController {
     @GetMapping("all")
     public PostResponse getAllPosts(
             @RequestParam(value = "pageNo", defaultValue = "0",required = false) int pageNo,
-            @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize
+            @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize,
+            @RequestParam( value = "sortBy", defaultValue = "id",required = false) String sortBy,
+            @RequestParam( value = "sortDir", defaultValue = "asc",required = false) String sortDir
     ){
-        return postService.getAllPosts(pageNo,pageSize);
+        return postService.getAllPosts(pageNo,pageSize,sortBy,sortDir);
     }
 
     // url : http://localhost:8080/api/posts/{id}
