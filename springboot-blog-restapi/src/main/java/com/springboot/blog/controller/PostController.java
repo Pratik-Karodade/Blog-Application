5a42fbc1ea9d2fe6b8b2df1510753194ad633244
Pratik-Karodade <karodadepratik@gmail.com>
2024-10-15 08:29:10 +0530

    latest commit

--- springboot-blog-restapi/src/main/java/com/springboot/blog/controller/PostController.java
+++ springboot-blog-restapi/src/main/java/com/springboot/blog/controller/PostController.java
@@ -4,6 +4,10 @@ import com.springboot.blog.payload.PostDto;
 import com.springboot.blog.payload.PostResponse;
 import com.springboot.blog.service.PostService;
 import com.springboot.blog.utils.AppConstants;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +18,9 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
+@Tag(
+        name = "CRUD operations for POST operations"
+)
 public class PostController {
 
     private PostService postService;
@@ -23,6 +30,9 @@ public class PostController {
     }
 
     // url : http://localhost:8080/api/posts
+    @Operation(summary = "Create post REST API",description = "Create post rest api is used to save post to database")
+    @ApiResponse(responseCode = "201",description = "Http status 201 created")
+    @SecurityRequirement(name = "Bearer Authentication")
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto){
@@ -32,6 +42,9 @@ public class PostController {
     // url : http://localhost:8080/api/posts/allposts
     // pagination and sorting
     @GetMapping("all")
+    @Operation(summary = "Get all posts REST API",
+            description = "Get all posts rest api is used to get all posts")
+    @ApiResponse(responseCode = "200",description = "Http status 200")
     public PostResponse getAllPosts(
             @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NO,required = false) int pageNo,
             @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
@@ -42,12 +55,19 @@ public class PostController {
     }
 
     // url : http://localhost:8080/api/posts/{id}
+    @Operation(summary = "Get post by id REST API",
+            description = "get post by id rest api is used to get post to database")
+    @ApiResponse(responseCode = "200",description = "Http status 200")
     @GetMapping("/{id}")
     public PostDto getPostById(@PathVariable(name = "id") long id){
         return postService.getPostById(id);
     }
 
     // url : http://localhost:8080/api/posts/{id}
+    @Operation(summary = "Update post REST API",
+            description = "Update post by id rest api is used to update post")
+    @ApiResponse(responseCode = "200",description = "Http status 200")
+    @SecurityRequirement(name = "Bearer Authentication")
     @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto,@PathVariable(name = "id") long id){
@@ -55,6 +75,10 @@ public class PostController {
     }
 
     // url : http://localhost:8080/api/posts/{id}
+    @Operation(summary = "Delete post REST API",
+            description = "Delete post by id rest api is used to delete post")
+    @ApiResponse(responseCode = "200",description = "Http status 200")
+    @SecurityRequirement(name = "Bearer Authentication")
     @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deletePost(@PathVariable (name = "id") long id){
